014b506328f9b06affad91de1fbc047d8f064c40
pscastillo <157133471+pscastillo@users.noreply.github.com>
2024-03-01 12:42:16 -0600

    semana 7

---
+++ semana 7/#semana7.py
@@ -0,0 +1,36 @@
+#Ejercicio1
+print("Ejercicio 1")
+nombre = input("Escriba su nombre")
+print("Hola,"+ nombre)
+
+#Ejercicio2
+print("Ejercicio 2")
+nombre = input("Escriba su nombre")
+edad = int(input("Escriba su edad"))
+print("Hola, !"+ nombre +"! Tienes "+ str(edad)+ "años")
+
+#Ejercicio3
+print("Ejercicio 3")
+Celsius = input("Ingrese la temperatura en grados celsius")
+formula = Celsius*9/5+32 
+print("La temperatura en Fahrenheit es de", formula)
+
+#Ejercicio4
+print("Ejercicio 4")
+numero1 = input("Ingrese un numero")
+numero2 = ("Ingrese otro numero")
+suma = numero1+numero2
+resta = numero1-numero2
+multiplicacion = numero1*numero2 
+division = numero1/numero2
+print=("La suma de los numeros es", suma,)
+print=("La resta de los numeros es",resta,)
+print=("La multiplicacion de los numeros es",multiplicacion,)
+print=("La division de los numeros es",division)
+
+#Ejercicio5 
+print("Ejercicio 5")
+base=int("Ingrese la base de su triangulo")
+altura=int("Ingrese la altura de su triangulo")
+area=base*altura/2
+print("El area del triangulo es de", area)
